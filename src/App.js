4d812a84c73d8f047c3ee9f305a900dd7216ea3c
Ross White <rosswhite@outlook.com>
2021-07-29 19:47:43 +0100

    React Routing to all pages working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import './App.css';
+import React from 'react'; 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/header';
+import About from './components/about';
+import Portfolio from "./components/portfolio";
+import Contact from './components/contact';
+import CV from './components/cv';
+
 
 function App() {
   return(
-    <Header />
+
+    <Router basename='/about'>
+      <Header />
+
+      <Router>
+        <Route path='/about' component={About} />
+      </Router>
+
+      <Router>
+        <Route path='/portfolio' component={Portfolio} />
+      </Router>
+
+      <Router>
+        <Route path='/cv' component={CV} />
+      </Router>
+
+      <Router>
+        <Route path='/contact' component={Contact} />
+      </Router>
+
+    </Router>
   );
 };
 

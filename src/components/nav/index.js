8f90325528fb3c421513c4b9b300f70ced877245
Ross White <rosswhite@outlook.com>
2021-07-31 16:00:40 +0100

    initial styling

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -3,18 +3,18 @@ import React from 'react';
 const Navigation = () => {
     return (
         <nav>
-            <ul className='space-x-4'>
-                <li className='inline-block'>
-                    <a href="/about">Home</a>
+            <ul className="list-reset lg:flex justify-end flex-1 items-center text-sm">                
+                <li className='mr-3'>
+                    <a className='inline-block text-gray-600 hover:text-gray-200 py-2 px-4' href="/about">Home</a>
                 </li>
-                <li className='inline-block'>
-                    <a href="/portfolio">Portfolio</a>
+                <li className='mr-3'>
+                    <a className='inline-block text-gray-600 hover:text-gray-200 py-2 px-4' href="/portfolio">Portfolio</a>
                 </li>
-                <li className='inline-block'>
-                    <a href="/contact">Contact</a>
+                <li className='mr-3'>
+                    <a className='inline-block text-gray-600 hover:text-gray-200 py-2 px-4' href="/contact">Contact</a>
                 </li>
-                <li className='inline-block'>
-                    <a href="/cv">CV</a>
+                <li className='mr-3'>
+                    <a className='inline-block text-gray-600 hover:text-gray-200 py-2 px-4' href="/cv">CV</a>
                 </li>
             </ul>
         </nav>

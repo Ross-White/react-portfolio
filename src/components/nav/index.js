a301f26ff86779537575475c304c520b6a5d6830
Ross White <rosswhite@outlook.com>
2021-07-31 17:04:19 +0100

    Header width and font size correct

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -3,18 +3,18 @@ import React from 'react';
 const Navigation = () => {
     return (
         <nav>
-            <ul className="list-reset lg:flex justify-end flex-1 items-center text-sm">                
+            <ul className="list-reset lg:flex justify-end flex-1 items-center text-base">                
                 <li className='mr-3'>
-                    <a className='inline-block text-gray-600 hover:text-gray-200 py-2 px-4' href="/about">Home</a>
+                    <a className='inline-block hover:text-gray-400' href="/about">Home</a>
                 </li>
                 <li className='mr-3'>
-                    <a className='inline-block text-gray-600 hover:text-gray-200 py-2 px-4' href="/portfolio">Portfolio</a>
+                    <a className='inline-block hover:text-gray-400' href="/portfolio">Portfolio</a>
                 </li>
                 <li className='mr-3'>
-                    <a className='inline-block text-gray-600 hover:text-gray-200 py-2 px-4' href="/contact">Contact</a>
+                    <a className='inline-block hover:text-gray-400' href="/contact">Contact</a>
                 </li>
                 <li className='mr-3'>
-                    <a className='inline-block text-gray-600 hover:text-gray-200 py-2 px-4' href="/cv">CV</a>
+                    <a className='inline-block hover:text-gray-400' href="/cv">CV</a>
                 </li>
             </ul>
         </nav>

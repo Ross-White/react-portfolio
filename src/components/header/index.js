ecb8780ce38dd654192e2a0224040f2e91166fef
Ross White <rosswhite@outlook.com>
2021-10-05 21:52:55 +0100

    adding responsive text to header/nav

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,7 +3,7 @@ import Navigation from '../nav';
 
 const Header = () => {
     return (
-        <header className='flex items-center place-content-between h-32 p-3 text-3xl m-0'>
+        <header className='text-2xl flex items-center place-content-between h-32 pl-8 md:text-5xl m-0'>
            <h1>Ross White Portfolio Page</h1>
            <Navigation />
         </header>
--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Navigation = () => {
     return (
         <nav>
-            <ul className="list-reset lg:flex justify-end flex-1 items-center text-base">                
+            <ul className="list-reset md:flex justify-end flex-1 items-center text-base md:text-xl mr-8">                
                 <li className='mr-3'>
                     <a className='inline-block hover:text-gray-400' href="/about">Home</a>
                 </li>

945d4bd33723a7c0219ac4155a3fc72032003987
Ross White <rosswhite@outlook.com>
2021-10-06 00:29:46 +0100

    fixing download

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -9,7 +9,7 @@ const Footer = () => {
             <a href="https://github.com/Ross-White" title="GitHub"><FontAwesomeIcon icon={faGithubSquare} size='4x' /></a>
             <a href="https://www.linkedin.com/in/ross-white-b4751814b/" title="LinkedIn"><FontAwesomeIcon icon={faLinkedin} size='4x' /></a>
             <a href="mailto:info@ross-white.co.uk" title="email"><FontAwesomeIcon icon={faEnvelope} size="4x" /></a>
-            <a href="public\images\Ross_White__CV.pdf" title="Download CV" download><FontAwesomeIcon icon={faDownload} size="4x" /></a>
+            <a href="./images/Ross_White__CV.pdf" title="Download CV" target="_blank" download><FontAwesomeIcon icon={faDownload} size="4x" /></a>
         </footer>
     )
 };

392bc9e0f97b18fbebc38fb291799677f7e0138f
Ross White <rosswhite@outlook.com>
2021-10-05 22:24:49 +0100

    adding mailto in footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Footer = () => {
@@ -7,6 +8,7 @@ const Footer = () => {
         <footer className="flex justify-center space-x-12">
             <a href="https://github.com/Ross-White"><FontAwesomeIcon icon={faGithubSquare} size='4x' /></a>
             <a href="https://www.linkedin.com/in/ross-white-b4751814b/"><FontAwesomeIcon icon={faLinkedin} size='4x' /></a>
+            <a href="mailto:info@ross-white.co.uk"><FontAwesomeIcon icon={faEnvelope} size="4x" /></a>
         </footer>
     )
 };

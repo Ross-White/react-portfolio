aeef515188937bf5f411a729014a939882d7454f
Ross White <rosswhite@outlook.com>
2021-11-22 18:46:41 +0000

    Adding Nav hover effects

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,8 +3,8 @@ import Navigation from '../nav';
 
 const Header = () => {
     return (
-        <header className='text-2xl flex items-center place-content-between h-32 pl-8 md:text-5xl m-0'>
-           <h1>Ross White Portfolio Page</h1>
+        <header className='text-2xl flex items-center place-content-between h-32 pl-32 md:text-5xl m-0'>
+           <h1>Ross White</h1>
            <Navigation />
         </header>
               

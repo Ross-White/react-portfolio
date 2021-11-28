e0adbde41417635984199168a4c015a9005d4fb9
Ross White <rosswhite@outlook.com>
2021-11-28 18:22:47 +0000

    restyling coaousel

--- src/components/project/index.js
+++ src/components/project/index.js
@@ -1,23 +1,33 @@
-import React from 'react';
+// import React from 'react';
 
-const Project = (props) => {
-  var image=`./images/${props.image}`
-  return (
-    <div key={props.index} className={props.index === props.currentProject ? "w-full h-80 flex md:flex-col md:w-1/4 rounded overflow-hidden shadow-lg my-s bg-scondary-color border-2" : 'hidden'}>
-      <div>
-        <img className="w-1/2 object-scale-down" src={image} alt={props.title} />
-        <div className="px-8 py-2">
-          <h1 className="font-bold text-xl mb-2">{props.title}</h1>
-          <p className="text-grey-darker text-base">{props.description}</p>
-          <p className="font-bold">{props.technology}</p>        
-        </div>
-        <div className="px-6 py-4">
-          <a href={props.url} className="inline-block bg-grey-lighter rounded-full px-3 py-1">Deployed App</a>
-          <a href={props.repo} className="inline-block bg-grey-lighter rounded-full px-3 py-1">Repo</a>
-        </div>
-      </div>
-    </div>
-  );
-}
+// const Project = (props) => {
+//   return (
+//     <img className={props.index === props.currentProject ? "transition duration-500 ease-in-out z-20 w-full h-80 flex md:flex-col md:w-2/3 md:h-2/3 rounded overflow-hidden shadow-lg my-s border-10 transform hover:scale-110 hover:shadow-2xl"
+//     : props.index === props.nextProject ? 'invisible transition duration-500 ease-in-out z-0 w-full h-80 flex md:visible md:flex-col md:w-1/2 md:h-1/2 rounded overflow-hidden shadow-lg my-s border-2 transform hover:scale-110 cursor-pointer'
+//       : props.index === props.prevProject ? 'invisible transition duration-500 ease-in-out z-0 w-full h-80 flex md:visible md:flex-col md:w-1/2 md:h-1/2 rounded overflow-hidden shadow-lg my-s border-2 transform hover:scale-110 cursor-pointer'
+//         : 'hidden'}
+//     onClick={props.changeProject(props.index)}
+//     src={`./images/${props.image}`}
+//     alt={props.title} 
+//     index={props.index}
+//     currentProject={JSON.stringify(props.currentProject)}/>
+//   );
 
-export default Project;
+// }
+
+// export default Project;
+
+{/* <div className={props.index === props.currenProject ? "w-full h-80 flex md:flex-col md:w-1/4 rounded overflow-hidden shadow-lg my-s bg-scondary-color border-2" : 'hidden'}>
+<div>
+  <img className="w-full object-scale-down" src={image} alt={props.title} />
+  <div className="px-8 py-2">
+    <h1 className="font-bold text-xl mb-2">{props.title}</h1>
+    <p className="text-grey-darker text-base">{props.description}</p>
+    <p className="font-bold">{props.technology}</p>        
+  </div>
+  <div className="px-6 py-4">
+    <a href={props.url} className="inline-block bg-grey-lighter rounded-full px-3 py-1">Deployed App</a>
+    <a href={props.repo} className="inline-block bg-grey-lighter rounded-full px-3 py-1">Repo</a>
+  </div>
+</div>
+</div> */}
--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -1,83 +1,51 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import ProjectData from '../../projectData';
-// import Project from '../../components/project';
-import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 
+const Portfolio = () => {
+  const last = ProjectData.length - 1;
+  // const [display, setDisplay] = useState([ProjectData[0], ProjectData[1], ProjectData[2]])
 
-class Portfolio extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentProject: 0,
-      paused: false,
-    };
+  const [currentProject, setCurrentProject] = useState(1);
+  const [prevProject, setPrevProject] = useState(0);
+  const [nextProject, setNextProject] = useState(2);
+
+  const changeProject = (event) => (index) => {
+    console.log(event);
+    let newProject = event;
+    setPrevProject(newProject -1 === 0 ? last : newProject - 1);
+    setCurrentProject(newProject);
+    setNextProject(newProject + 1 === last ? 0 : newProject + 1);
+    console.log(prevProject, currentProject, nextProject)
+    // setDisplay([ProjectData[prevProject], ProjectData[currentProject], ProjectData[nextProject]]);
+    // console.log(display);
   }
 
-  componentDidMount() {
-      if (this.state.paused === false) {
-        let newProject =
-          this.newProject === ProjectData.length - 1 ? 0 :
-          this.state.currentProject + 1;
-        this.setState({ currentProject: newProject });
-      }
-  }
-
-  nextProject = () => {
-    let newProject =
-      this.state.currentProject === ProjectData.length - 1
-        ? 0
-        : this.state.currentProject + 1;
-    this.setState({ currentProject: newProject });
-  };
-
-  prevProject = () => {
-    let newProject =
-      this.state.currentProject === 0
-        ? ProjectData.length - 1
-        : this.state.currentProject - 1;
-    this.setState({ currentProject: newProject });
-  };
-
-  setCurrentProject = (index) => {
-    this.setState({ currentProject: index });
-  };
-
-  render() {
-    return (
-      <div className="flex flex-col md:flex-row md:w-2/3 flex-wrap justify-center items-center space-x-5 space-y-5 place-items-center" >
-        <AiOutlineLeft onClick={this.prevProject} className='absolute left-8 text-3xl inset-y-1/2 bg-black text-white cursor-pointer' />
-        {ProjectData.map((project, index) => {
-          return (
-            // <Project 
-            //   key={index} 
-            //   currentProject={this.state.currentProject} 
-            //   title={project.title}
-            //   description={project.description} 
-            //   image={project.image} 
-            //   technology={project.technology}
-            //   url={project.url} 
-            //   repo={project.repo} />
-            <div key={index} className={index === this.state.currentProject ? "w-full h-80 flex md:flex-col md:w-2/3 md:h-2/3 rounded overflow-hidden shadow-lg my-s bg-scondary-color border-2" : 'hidden'}>
-              <div className="flex md:flex-col justify-center">
-                <img className="w-1/2 object-scale-down" src={`./images/${project.image}`} alt={project.title} />
-                <div className="px-8 py-2">
-                  <h1 className="font-bold text-xl mb-2">{project.title}</h1>
-                  <p className="text-grey-darker text-base">{project.description}</p>
-                  <p className="font-bold">{project.technology}</p>
-                </div>
-                <div className="px-6 py-4">
-                  <a href={project.url} className="inline-block bg-grey-lighter rounded-full px-3 py-1">Deployed App</a>
-                  <a href={project.repo} className="inline-block bg-grey-lighter rounded-full px-3 py-1">Repo</a>
-                </div>
-              </div>
-              <AiOutlineRight onClick={this.nextProject} className='absolute right-8 text-3xl inset-y-1/2 bg-black text-white cursor-pointer' />
+  return (
+     <div className="flex flex-col md:flex-row md:w-2/3 justify-center items-center space-x-5 space-y-5 place-items-center" >
+      {ProjectData.map((project, index) => (
+        <div className={index === currentProject ? "transition duration-500 ease-in-out z-20 w-full h-80 flex md:flex-col md:w-full md:h-2/3 rounded overflow-hidden my-s border-10 transform hover:scale-110 hover:shadow-2xl z-10"
+          : index === prevProject ? 'invisible transition duration-500 ease-in-out z-0 w-full h-80 flex md:visible md:flex-col md:w-1/2 md:h-1/3 rounded overflow-hidden shadow-lg my-s border-2 transform hover:scale-110 cursor-pointer relative z-0'
+            : index === nextProject ? 'invisible transition duration-500 ease-in-out z-0 w-full h-80 flex md:visible md:flex-col md:w-1/2 md:h-1/3 rounded overflow-hidden shadow-lg my-s border-2 transform hover:scale-110 cursor-pointer relative z-0'
+              : 'hidden'}
+          onClick={changeProject(index)}>
+          <img
+            className='h-full w-full shadow-xl'
+            src={`./images/${project.image}`}
+            alt={project.title} />
+          <section className={index === currentProject ? 'visible mt-8' : 'hidden'}>
+            <h2>{project.title}</h2>
+            <p>{project.description}</p>
+            <p>{project.technology}</p>
+            <div className="px-6 py-4">
+              <a href={project.url} className="inline-block bg-grey-lighter rounded-full px-3 py-1">Deployed App</a>
+              <a href={project.repo} className="inline-block bg-grey-lighter rounded-full px-3 py-1">Repo</a>
             </div>
-          );
-        }
-        )}
-      </div>
-    );
-  }
+          </section>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Portfolio;
+
